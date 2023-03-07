de9d2de5b86a820c6a242ffcc19d29012d323787
AnutaUser <bezkorovainaanna048@gmail.com>
2023-03-07 21:12:37 +0200

    less_4_

--- src/app.ts
+++ src/app.ts
@@ -1,68 +1,29 @@
-import express, { Request, Response } from "express";
+import { config } from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 
-import { User } from "./models/User.model";
-import { IUser } from "./types/user.types";
+import { configs } from "./configs/config";
+import { userRouter } from "./routes/user.router";
+import { IError } from "./types/common.types";
 
 const app = express();
+
+config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get(
-  "/users",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
-    const users = await User.find();
-    return res.json(users);
-  }
-);
+app.use("/users", userRouter);
+app.use((err: IError, req: Request, res: Response, next: NextFunction) => {
+  const status = err.status;
 
-app.get(
-  "/users/:userId",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
-    const { userId } = req.params;
-    const user = await User.findById(userId);
-
-    return res.json(user);
-  }
-);
-
-app.post("/users", async (req: Request, res: Response) => {
-  const body = req.body;
-
-  const user = await User.create(body);
-
-  res.status(201).json({
-    message: "user created",
-    data: user,
+  return res.status(status).json({
+    message: err.message,
+    status,
   });
 });
 
-app.patch("/users/:userId", async (req: Request, res: Response) => {
-  const { userId } = req.params;
-  const body = req.body;
-
-  const user = await User.updateOne({ _id: userId }, { ...body });
-
-  res.status(200).json({
-    message: "User updated",
-    data: user,
-  });
-});
-
-app.delete("/users/:userId", async (req: Request, res: Response) => {
-  const { userId } = req.params;
-
-  await User.deleteOne({ _id: userId });
-
-  res.sendStatus(200).json({
-    message: "User deleted",
-  });
-});
-
-const PORT = 4444;
-
-app.listen(PORT, () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/sep_2022").then();
-  console.log(`Server started on port: ${PORT}`);
+app.listen(configs.PORT, () => {
+  mongoose.connect(configs.API_URL).then();
+  console.log(`Server started on port: ${configs.PORT}`);
 });
